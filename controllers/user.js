b453bc3ca487eb0e5c325574476b12933ede94bf
erikgonzalez <erik.gonzalez.ac@gmail.com>
2019-12-26 19:08:21 -0700

    Método para obtener usuarios.

--- controllers/user.js
+++ controllers/user.js
@@ -344,6 +344,40 @@ var controller = {
                 });
             }
         });
+    },
+
+    getUsers: function(req, res){
+        User.find().exec((err, users)=>{
+            if(err || !users){
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'No existen usuarios'
+                });
+            }
+
+            return res.status(200).send({
+                status: 'success',
+                users
+            });
+        });
+    },
+
+    getUser: function(req, res){
+        var userId = req.params.userId;
+
+        User.findById(userId).exec((err, user)=>{
+            if(err || !user){
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'No existe el usuario'
+                });
+            }
+
+            return res.status(200).send({
+                status: 'success',
+                user
+            });
+        });
     }
 
 
