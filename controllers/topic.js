b22ceceeb4a53f588b7ce100265de74d2842678c
erikgonzalez <erik.gonzalez.ac@gmail.com>
2020-01-02 19:06:54 -0700

    Metodos para guardar topics y obtener topics.

--- controllers/topic.js
+++ controllers/topic.js
@@ -1,5 +1,9 @@
 'use strict'
 
+var validator = require('validator');
+var Topic = require('../models/topic');
+
+
 var controller = {
 
    test: function(req, res) {
@@ -7,7 +11,135 @@ var controller = {
          message: 'topic!!'
        });
        
+   },
+
+   save: function(req,res){
+
+    // Recoger parametros por post
+    var params = req.body;
+    
+    // Validar los datos
+    try{
+
+      var validate_title = !validator.isEmpty(params.title);
+      var validate_content = !validator.isEmpty(params.title);
+      var validate_lang = !validator.isEmpty(params.title);
+
+    }catch(err){
+      return res.status(200).send({
+        message: 'Faltan datos por enviar'
+      });
+    }
+
+    if( validate_title && validate_content && validate_lang){
+      // Crear objeto a guardar
+      var topic = new Topic();
+
+      // Asignar valores
+      topic.title = params.title;
+      topic.content = params.content;
+      topic.code = params.code;
+      topic.lang = params.lang;
+      topic.user = req.user.sub;
+
+      // Guardar el topic
+      topic.save((err, topicStored)=>{
+          if(err || !topicStored){
+            res.status(404).send({
+              status: 'error',
+              message: 'No ha sido posible guardar el topic'
+            });
+          }
+        // devolver una respuesta
+          return res.status(200).send({
+            status: 'success',
+            message: 'Topic guardado correctamente',
+            topic: topicStored
+          });
+      });
+
+      
+    }else{
+      return res.status(200).send({
+        message: 'Datos no validos'
+      });
+    }
+   },
+
+   getTopics: function(req, res){
+    // Cargar la libreria de paginación en la clase (modelo)
+    // Recoger la pagina actual
+    
+    if(req.params.page == null || req.params.page ==0 || req.params.page == "0" || req.params.page == undefined || !req.params.page){
+      page = 1;
+    }else{
+      var page = parseInt(req.params.page);
+    }
+
+    // Indicar las opciones de paginación
+    var options = {
+      sort: { date: -1 },
+      populate:'user',
+      limit: 5,
+      page: page
+    }
+    // Find paginado
+    Topic.paginate({},options, (err,topics)=>{
+        // Devolver resultado(topics, total de topic, total de paginas)
+
+        if(err){
+          return res.status(500).send({
+            status: 'error',
+            message: 'Error al obtener los topics'
+          });
+        }
+
+        if(!topics){
+          return res.status(404).send({
+            status: 'error',
+            message: 'No fue posible obtener los topics'
+          });
+        }
+
+        return res.status(200).send({
+          status: 'success',
+          topics: topics.docs,
+          totalDocs: topics.totalDocs,
+          totalPages: topics.totalPages
+        });
+    });
+    
+   },
+
+   getTopicsByUser: function (req, res) {
+    // Conseguir el id del usuario
+    var userId = req.params.user;
+    // find con la condicion del usuario
+    Topic.find({
+      user:userId
+    }).sort([['date', 'descending']]).exec((err,topics)=>{
+      if(err){
+        return res.status(500).send({
+          status: 'error',
+          message: 'Error en la petición'
+        });
+      }
+      if(!topics){
+        return res.status(404).send({
+          status: 'error',
+          message: 'No hay topics para mostrar'
+        });
+      }
+        // Devolver un resultado. 
+        return res.status(200).send({
+           status: 'success',
+           topics
+        });
+
+    });
+   
    }
+
     
 };
 
--- routes/topic.js
+++ routes/topic.js
@@ -7,5 +7,8 @@ var router = express.Router();
 var md_auth = require('../middleware/authenticated');
 
 router.get('/test', TopicController.test);
+router.post('/topic',md_auth.authenticated, TopicController.save);
+router.get('/topics/:page?', TopicController.getTopics);
+router.get('/user-topics/:user',TopicController.getTopicsByUser);
 
 module.exports =  router;
